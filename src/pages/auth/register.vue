ddc548e821769a819d51174259d714c960d6eb85
Beekmuhammad <161308700+BekmuhammadDev@users.noreply.github.com>
2025-06-25 22:16:43 +0500

    register update

---
+++ src/pages/auth/register.vue
@@ -0,0 +1,105 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const form = ref({
+    email: '',
+    hasPromoCode: '',
+    firstName: '',
+    lastName: '',
+    promoCode: '',
+    username: '',
+    password: '',
+    confirmPassword: '',
+    jsn: '',
+    city: '',
+});
+
+const handleRegister = () => {
+    console.log('Registering:', form.value);
+};
+</script>
+
+<template>
+    <div class=" flex flex-col items-center mt-10 justify-center bg-white">
+        <h2 class="text-[40px] text-[#000000] font-normal mb-8">Регистрация</h2>
+
+        <form @submit.prevent="handleRegister" class="w-full max-w-4xl grid grid-cols-2 gap-7 px-10">
+
+            <div>
+                <label for="" class=" ml-2">Email</label>
+                <input v-model="form.email" placeholder="email" class="border w-full rounded-3xl px-4 py-3" />
+            </div>
+
+
+            <div>
+                <label for="" class=" ml-2">Логин</label>
+                <input v-model="form.username" placeholder="Логин" class="border w-full rounded-3xl px-4 py-3" />
+            </div>
+
+            <div>
+                <label for="" class=" ml-2">Есть промокод?</label>
+                <input v-model="form.hasPromoCode" placeholder="Есть промокод?"
+                    class="border w-full rounded-3xl px-4 py-3" />
+            </div>
+
+            <div>
+                <label for="" class=" ml-2">Пароль</label>
+                <input v-model="form.password" type="password" placeholder="Пароль"
+                    class="border w-full rounded-3xl px-4 py-3" />
+            </div>
+
+            <div>
+                <label for="" class=" ml-2">Имя</label>
+                <input v-model="form.firstName" placeholder="Имя" class="border w-full rounded-3xl px-4 py-3" />
+            </div>
+
+            <div>
+                <label for="" class=" ml-2">Повторите пароль</label>
+                <input v-model="form.confirmPassword" type="password" placeholder="Повторите пароль"
+                    class="border w-full rounded-3xl px-4 py-3" />
+            </div>
+
+            <div>
+                <label for="" class=" ml-2">Фамилия</label>
+                <input v-model="form.lastName" placeholder="Фамилия" class="border w-full rounded-3xl px-4 py-3" />
+            </div>
+
+            <div>
+                <label for="" class=" ml-2">JShShIR</label>
+                <input v-model="form.jsn" placeholder="JSHSHIR" class="border w-full rounded-3xl px-4 py-3" />
+            </div>
+
+            <div>
+                <label for="" class=" ml-2">Партнёрский код</label>
+                <input v-model="form.promoCode" placeholder="Партнерский код"
+                    class="col-span-2 border w-full rounded-3xl px-4 py-3" />
+            </div>
+
+            <div>
+                <label for="" class=" ml-2">Серия</label>
+                <input v-model="form.city" placeholder="Город" class="col-span-2 border w-full rounded-3xl px-4 py-3" />
+            </div>
+        </form>
+
+        <div class=" bg-green-800 w-full  mt-10">
+            <div class="col-span-2 flex flex-col items-center py-10 gap-4">
+                <button type="submit" class="bg-white rounded-2xl text-[#064E3B] px-6 py-2 w-full max-w-sm">
+                Зарегистрироваться
+            </button>
+
+            <div class="flex items-center gap-4">
+                <div class="flex-grow w-[150px] h-px bg-white/40"></div>
+                <div class="text-white text-sm">или</div>
+                <div class="flex-grow w-[150px] h-px bg-white/40"></div>
+            </div>
+
+
+            <button type="button"
+                class="border bg-white text-black rounded-2xl py-2 px-4 w-full max-w-sm flex items-center justify-center gap-2 ">
+                <img src="https://www.svgrepo.com/show/475656/google-color.svg" alt="Google" class="h-5" />
+                Продолжить с Google
+            </button>
+            </div>
+        </div>
+    </div>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
+
 import appLayout from '@/layouts/appLayout.vue';
 
 import HomePage from '@/pages/Home.vue';
@@ -6,16 +7,28 @@ import ProfilePage from '@/pages/Profile.vue';
 import CoursesPage from '@/pages/Course.vue';
 import BalancePage from '@/pages/Balance.vue';
 
+import LoginPage from '@/pages/auth/login.vue';
+import RegisterPage from '@/pages/auth/register.vue';
+
 const routes = [
+
+  { path: '/login', name: 'login', component: LoginPage },
+  { path: '/register', name: 'register', component: RegisterPage },
+
   {
     path: '/',
     component: appLayout,
     children: [
-      { path: 'home', name: 'home', component: HomePage }, 
+      { path: 'home', name: 'home', component: HomePage },
       { path: 'profile', name: 'profile', component: ProfilePage },
       { path: 'courses', name: 'courses', component: CoursesPage },
       { path: 'balance', name: 'balance', component: BalancePage },
     ],
+  },
+
+  {
+    path: '/',
+    redirect: '/home',
   },
 ];
 
